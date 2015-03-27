955c1a85fa7d3cd474ab9de11430d3da2c4dcd2d
Kyle <thibau13@unlv.nevada.edu>
2015-03-27 12:41:13 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScavengerHunt::Application.config.secret_key_base = '168b7c86b64784efed1be58f60169e4206a4952ebbc960ff7c8f9743ac9f29c68f86169bfecd0df13c0ec805a2ce9d55a8c8699ab6abd27c9301003b045f72bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScavengerHunt::Application.config.session_store :cookie_store, key: '_ScavengerHunt_session'
